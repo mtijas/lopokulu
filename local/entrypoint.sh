12223070ffab165213c45907b024d3dfbb84ffcd
Markus Ijäs <markus.ijas@outlook.com>
2021-09-23 17:07:24 +0300

    Adds sleep to lopokuluapp entrypoint to accommodate postgres startup time and adds local dev data sql dump. Fixes LOP-33

--- local/entrypoint.sh
+++ local/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+sleep 5
 python3 /app/manage.py makemigrations --no-input
 python3 /app/manage.py migrate
 python3 manage.py runserver 0.0.0.0:8000
---
+++ local/local-dev-data.sql
@@ -0,0 +1,2 @@
+INSERT INTO "auth_user" ("id", "password", "last_login", "is_superuser", "username", "first_name", "last_name", "email", "is_staff", "is_active", "date_joined") VALUES
+(1,	'pbkdf2_sha256$260000$7WXt5WC97Qqd6yCfQYka9M$s3m2DwMrNylYVpQ7LYOgTewtLVTnKfZS/1L1e/Kqn6w=',	'2021-09-23 13:16:14.371703+00',	'1',	'admin',	'',	'',	'',	'1',	'1',	'2021-09-23 13:14:55.180726+00');
