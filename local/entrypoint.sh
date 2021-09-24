eb0cf0124c3bed1387d68ce823231d0d80b7d065
Markus Murto <markus@murto.eu>
2021-09-24 10:37:04 +0300

    Switch app directory to src

--- local/entrypoint.sh
+++ local/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 sleep 5
-python3 /app/manage.py makemigrations --no-input
-python3 /app/manage.py migrate
-python3 /app/manage.py runserver 0.0.0.0:8000
+python3 /src/manage.py makemigrations --no-input
+python3 /src/manage.py migrate
+python3 /src/manage.py runserver 0.0.0.0:8000
--- testing/entrypoint.sh
+++ testing/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 sleep 5
-python3 /app/manage.py collectstatic
-gunicorn --chdir "/app" lopokulu.wsgi:application --bind 0.0.0.0:8000
+python3 /src/manage.py collectstatic
+gunicorn --chdir "/src" lopokulu.wsgi:application --bind 0.0.0.0:8000
