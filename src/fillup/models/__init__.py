56de9f76530d502c0562de32559e546d14b1d098
Markus Murto <markus@murto.eu>
2021-10-01 10:00:54 +0300

    Add few more copyright information

--- src/fillup/models/__init__.py
+++ src/fillup/models/__init__.py
@@ -1,3 +1,9 @@
+# SPDX-FileCopyrightText: 2021 Jani Lehtinen
+# SPDX-FileCopyrightText: 2021 Markus Ijäs
+# SPDX-FileCopyrightText: 2021 Markus Murto
+#
+# SPDX-License-Identifier: MIT
+
 from .fillups import Fillup
 from .vehicles import Vehicle
 from .vehicles_users import VehicleUser
