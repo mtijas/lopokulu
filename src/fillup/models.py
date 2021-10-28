49ee7f0feb75c5d25b3e39a8bc8b909e11337f7e
Markus Ijäs <markus.ijas@outlook.com>
2021-10-28 21:28:04 +0300

    Adds FK Person and Addition date to Fillup model [LOP-55, LOP-56]

--- src/fillup/models.py
+++ src/fillup/models.py
@@ -6,8 +6,10 @@
 
 from django.core.exceptions import ValidationError
 from django.db import models
+from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 from manager.models import Vehicle
+from manager.models import Person
 
 def validate_positive(value):
     if value <= 0:
@@ -19,8 +21,15 @@ class Fillup(models.Model):
         "Price per unit", decimal_places=3, max_digits=5, validators=[validate_positive])
     amount = models.FloatField("Total filled up amount", validators=[validate_positive])
     distance = models.FloatField("Total odometer")
+    addition_date = models.DateTimeField(default=timezone.now)
 
     vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
+    person = models.ForeignKey(
+        Person,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
 
     class Meta:
         ordering = ['-id']
