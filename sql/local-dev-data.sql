8f7cb423991a732f3767d3883277bf442d740dab
Markus Ijäs <41751327+mtijas@users.noreply.github.com>
2021-11-17 22:27:08 +0200

    Update local-dev-data.sql

--- sql/local-dev-data.sql
+++ sql/local-dev-data.sql
@@ -8,7 +8,7 @@ INSERT INTO "manager_person" ("id", "password", "last_login", "is_superuser", "e
 (1,	'pbkdf2_sha256$260000$HvWDennzaccvs6vV7sPhxc$7h4QgeoLRpHKgbCmpF0w9NWyNMtKkv5Yd3FhYvjIyRg=',	'2021-10-14 08:12:58.382745+00',	'1',	'lopokulu@foo.bar',	'1',	'1',	'1');
 
 INSERT INTO "manager_vehicle" ("id", "register_number", "name") VALUES
-(1,	'OIO-654',	'Hieno punainen Golf'),
+(1,	'CBA-321',	'Ensimmäinen auto'),
 (2,	'ABC-123',	'Toinen auto');
 
 INSERT INTO "manager_vehicleuser" ("id", "role", "person_id", "vehicle_id") VALUES
