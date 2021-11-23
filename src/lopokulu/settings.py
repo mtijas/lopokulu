6b4dcf452f6524eac041811b41c7427efdb4c47c
Markus Ijäs <markus.ijas@outlook.com>
2021-11-23 11:29:32 +0200

    Updates main navigation layout and colors [LOP-75]

--- src/lopokulu/settings.py
+++ src/lopokulu/settings.py
@@ -137,6 +137,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "../static"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
