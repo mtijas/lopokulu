2012926ce911805bedabb5a13e61214dc91f928e
Markus Ijäs <markus.ijas@outlook.com>
2021-12-16 21:01:10 +0200

    Fixes navigation active

---
+++ src/fillup/templatetags/fillup_extras.py
@@ -0,0 +1,16 @@
+# SPDX-FileCopyrightText: 2021 Jani Lehtinen
+# SPDX-FileCopyrightText: 2021 Markus Ijäs
+# SPDX-FileCopyrightText: 2021 Markus Murto
+#
+# SPDX-License-Identifier: MIT
+
+from django import template
+
+register = template.Library()
+
+@register.simple_tag
+def active(request, pattern):
+    import re
+    if re.search(pattern, request.path):
+        return 'active'
+    return ''
