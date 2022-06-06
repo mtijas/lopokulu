615e5cf3d99aed3d1e76869f9a78baf43d516800
Markus Ijäs <markus.ijas@outlook.com>
2022-06-06 20:56:56 +0300

    Adds rudimentary measurement type selection on equipment forms

--- src/equipment/forms.py
+++ src/equipment/forms.py
@@ -3,19 +3,31 @@
 # SPDX-License-Identifier: MIT
 
 from django import forms
+from django.conf import settings
 from django.core.exceptions import ObjectDoesNotExist, ValidationError
 from django.utils.translation import gettext as _
 
 from .models import Equipment
 
+MEASUREMENT_CHOICES = []
+for app in settings.INSTALLED_MEASUREMENT_APPS:
+    MEASUREMENT_CHOICES.append((app, app))
+
 
 class EquipmentForm(forms.ModelForm):
+    # @TODO: Unit tests
+    allowed_measurements = forms.MultipleChoiceField(
+        required=False,
+        widget=forms.CheckboxSelectMultiple,
+        choices=MEASUREMENT_CHOICES,
+    )
+
     def __init__(self, user, *args, **kwargs):
         self.user = user
         super(EquipmentForm, self).__init__(*args, **kwargs)
 
     class Meta:
         model = Equipment
-        fields = ["name", "register_number"]
+        fields = ["name", "register_number", "allowed_measurements"]
 
     field_order = ["name", "register_number"]
