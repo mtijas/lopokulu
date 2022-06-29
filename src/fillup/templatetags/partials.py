821ff3e34d8fed5b1b0c617d3249042bb7237dbf
Markus Ijäs <markus.ijas@outlook.com>
2022-06-29 20:40:06 +0300

    Adds charts for fillups!

--- src/fillup/templatetags/partials.py
+++ src/fillup/templatetags/partials.py
@@ -2,6 +2,8 @@
 #
 # SPDX-License-Identifier: MIT
 
+import json
+
 from datetime import timedelta
 
 from django import template
@@ -27,6 +29,7 @@ def fillup_equipment_stats(equipment, start_dt=31, stop_dt=now(), mode: str = No
         "start_dt": start_dt,
         "stop_dt": stop_dt,
         "mode": mode,
+        "chart": None,
         "stats": {
             "consumption__avg": 0,
             "total_price__sum": 0,
@@ -39,10 +42,71 @@ def fillup_equipment_stats(equipment, start_dt=31, stop_dt=now(), mode: str = No
 
     fillups = Fillup.objects.filter(
         addition_date__gte=start_dt, addition_date__lte=stop_dt, equipment=equipment
-    )
+    ).order_by("addition_date")
 
     if not fillups:
         return data
+
+    labels = []
+    consumption_data = []
+    price_data = []
+    for fillup in fillups:
+        labels.append(fillup.addition_date.strftime("%d.%-m."))
+        consumption_data.append(fillup.consumption)
+        price_data.append(str(fillup.price))
+
+    data["chart"] = {
+        "labels": json.dumps(labels),
+        "datasets": json.dumps(
+            [
+                {
+                    "label": "Consumption (l/100km)",
+                    "data": consumption_data,
+                    "fill": True,
+                    "borderColor": "rgb(75, 192, 192)",
+                    "backgroundColor": "rgb(198, 235, 235, 0.25)",
+                    "spanGaps": True,
+                    "yAxisID": "y1"
+                },
+                {
+                    "label": "Price (€/l)",
+                    "data": price_data,
+                    "fill": True,
+                    "borderColor": "rgb(192, 75, 192)",
+                    "backgroundColor": "rgb(235, 198, 235, 0.25)",
+                    "spanGaps": True,
+                    "yAxisID": "y2"
+                },
+            ]
+        ),
+        "options": json.dumps(
+            {
+                "scales": {
+                    "y1": {
+                        "type": "linear",
+                        "display": True,
+                        "position": "left",
+                        "title": {
+                            "display": True,
+                            "text": "l/100km"
+                        }
+                    },
+                    "y2": {
+                        "type": "linear",
+                        "display": True,
+                        "position": "right",
+                        "grid": {
+                            "drawOnChartArea": False,
+                        },
+                        "title": {
+                            "display": True,
+                            "text": "€/l"
+                        }
+                    },
+                },
+            }
+        )
+    }
 
     data["stats"] = fillups.aggregate(
         Avg("consumption"), Sum("total_price"), Sum("distance_delta")
--- src/static/lopokulu/js/core.js
+++ src/static/lopokulu/js/core.js
@@ -1,8 +1,11 @@
 /* Löpökulu Core JS */
 
+const charts = [];
+
 window.addEventListener("load", () => {
     attachAreYouSure();
     attachDTPicker();
+    attachCharts();
 });
 
 /* Attaches "are you sure" dialog to all elements with data-confirm attribute */
@@ -33,5 +36,35 @@ function attachDTPicker() {
             options["time_24hr"] = true;
         }
         flatpickr(element, options);
+    });
+}
+
+/* Attaches ChartJS charts to every element with data-chartjs.
+
+Attributes:
+data-chartjs - Enables ChartJS for the element
+data-labels - Array of labels (x axis)
+data-sets - Array of ChartJS datasets
+data-options - Dict of ChartJS options
+data-type - Chart type
+*/
+function attachCharts() {
+    let id_num = 0;
+    document.querySelectorAll("[data-chartjs]").forEach((element) => {
+        let id = "chart_" + id_num;
+        element.id = id;
+
+        let chart = new Chart(id, {
+            type: element.getAttribute("data-type"),
+            data: {
+                labels: JSON.parse(element.getAttribute("data-labels")),
+                datasets: JSON.parse(element.getAttribute("data-sets")),
+            },
+            options: JSON.parse(element.getAttribute("data-options")),
+        });
+
+        charts.push(chart);
+
+        id_num++;
     });
 }
