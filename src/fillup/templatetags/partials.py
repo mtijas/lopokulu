75a130b6c199d6055c4675df896bfc72662c03fc
Markus Ijäs <markus.ijas@outlook.com>
2022-11-30 16:31:14 +0200

    Removes now() from fillup_equipment_stats function definition. Resolves #2.

--- src/fillup/templatetags/partials.py
+++ src/fillup/templatetags/partials.py
@@ -16,8 +16,11 @@ register = template.Library()
 
 
 @register.inclusion_tag("fillup/tags/equipment_stats.html")
-def fillup_equipment_stats(equipment, start_dt=31, stop_dt=now(), mode: str = None):
+def fillup_equipment_stats(equipment, start_dt=31, stop_dt=None, mode: str = None):
     """Collects fillup stats for datetime range for specific equipment"""
+    if stop_dt is None:
+        stop_dt = now()
+
     if mode == "current year":
         start_dt = now().replace(day=1, month=1)
     elif type(start_dt) is int:
